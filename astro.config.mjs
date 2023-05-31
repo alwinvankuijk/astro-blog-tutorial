997666dceda4a40e5696c90342f2d018776d5b7e
alwinvankuijk <alwinvankuijk@gmail.com>
2023-05-31 23:21:32 +0200

    Added two new pages - about and blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://alwinvankuijk.github.io',
-  // base: '/astro-blog-tutorial',
+  base: '/astro-blog-tutorial',
 });
