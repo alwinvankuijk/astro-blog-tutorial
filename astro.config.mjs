8f5abfa4ba606d366cabc5cce9a2dd2aba9a4e53
alwinvankuijk <alwinvankuijk@gmail.com>
2023-06-01 10:16:16 +0200

    finished the tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://alwinvankuijk.github.io',
   base: '/astro-blog-tutorial',
+  integrations: [preact()]
 });
